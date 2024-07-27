4068f5bd2b09c31e50aefcbeebd69cec1872c934
Ayushjodd <ayushkalonia2016@gmail.com>
2024-07-27 21:08:25 +0530

    finish the footer component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wraper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assests/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2024 sasta-luma no rights reserved😂</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
